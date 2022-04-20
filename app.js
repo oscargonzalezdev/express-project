6addb27e51a61e7a0e8b4757620aa4d7f4d86072
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-04-20 14:29:11 +0200

    add layouts and images

--- app.js
+++ app.js
@@ -4,19 +4,67 @@ const express = require('express');
 const { path } = require('express/lib/application');
 const app = express();
 
+// handelbars 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
 
-app.get('/home', (req, res, next)=>{
-    res.sendFile(__dirname+'/views/index.html');
+// routes
+app.get('/', (req, res, next)=>{
+    // res.sendFile(__dirname+'/views/index.html');
+    res.render('index');
 } );
 
 app.get('/about', (req, res, next)=>{
-    res.sendFile(__dirname+'/views/about.html');
+    // res.sendFile(__dirname+'/views/about.html');
+    res.render('about');
 } );
 
 app.get('/contact', (req, res, next)=>{
-    res.sendFile(__dirname+'/views/contact.html');
+    // res.sendFile(__dirname+'/views/contact.html');
+    res.render('contact');
 } );
+
+// products
+
+
+app.get("/bike1", (req, res, next) => {
+
+    const data = {
+        title: "Bike1",
+        price: 700,
+        imageFile: "bike1.jpeg",
+        stores: ["Online", "Albacete", "Freiburg", "Amsterdam"]
+    }
+
+    res.render("product", data);
+});
+
+app.get("/bike2", (req, res, next) => {
+
+    const data = {
+        title: "Bike2",
+        price: 900,
+        imageFile: "bike2.jpeg"
+    }
+
+    res.render("product", data);
+});
+
+app.get("/bike3", (req, res, next) => {
+
+    const data = {
+        title: "Bike3",
+        price: 1300,
+        imageFile: "bike3.jpeg"
+    }
+
+    res.render("product", data);
+});
+
+
+
 
 app.listen(3000, () => {
     console.log('server listening to request...');
